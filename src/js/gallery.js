04bbe244e0e806fb4b6cebe367dfbfe14f7b4e4b
Yunus Özkaya <92160164+YunusOzkaya@users.noreply.github.com>
2025-06-11 11:29:18 +0300

    Update gallery.js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,6 +1,27 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+
+
+const API_KEY = '50136749-dac3218f1634fe54e8a0614ce';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function fetchImages(query) {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+
+  const response = await fetch(`${BASE_URL}?${params}`);
+  const data = await response.json();
+  return data.hits;
+}
+
+
+
 let lightbox;
 
 export function renderGallery(images) {
