5aa802f13c3b157005c4262283a5ea5a859730cf
Yunus Özkaya <92160164+YunusOzkaya@users.noreply.github.com>
2025-06-12 22:36:14 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,21 @@
+import axios from 'axios';
+
 const API_KEY = '50136749-dac3218f1634fe54e8a0614ce';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query) {
-  const params = new URLSearchParams({
+  const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
-    safesearch: 'true',
-  });
+    safesearch: true,
+  };
 
-  const response = await fetch(`${BASE_URL}?${params}`);
-  const data = await response.json();
-  return data.hits;
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response.data.hits;
+  } catch (error) {
+    throw new Error('Image fetch failed');
+  }
 }
